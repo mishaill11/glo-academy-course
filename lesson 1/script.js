d903a88f43904e73ba3da58c380f2f5ee71fb9ce
mishaill11 <mpkhayko@mail.ru>
2019-08-22 13:39:00 +0700

    Lesson01

---
+++ lesson 1/script.js
@@ -0,0 +1,3 @@
+let money = 'dollar', income = 10, addExpenses = 20, deposit = 30, mission = 'improve your', period = 'six weeks';
+alert(mission + ' JS for ' + period);
+console.log('money: ', money);
