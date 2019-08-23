bb620330223803858550754b572ec54c142b4a57
mishaill11 <mpkhayko@mail.ru>
2019-08-23 15:54:04 +0700

    add complicated task

---
+++ lesson 2/script_complicated.js
@@ -0,0 +1,8 @@
+let num = 266219;
+num += '';
+num.split('');
+num = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+console.log(num);
+console.log(num **= 3);
+num += '';
+console.log(num = num[0] + num[1]);
