2431fcdee782d7c395ce16e3fe3936515ec44d65
mishaill11 <mpkhayko@mail.ru>
2019-08-24 01:57:53 +0700

    поменял код js

--- lesson 2/script_complicated.js
+++ lesson 2/script_complicated.js
@@ -1,8 +1,11 @@
 let num = 266219;
-num += '';
-num.split('');
-num = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+num = num.toString();
+num = num.split('');
+console.log('num: ', num);
+num = num.reduce(function(sum, current) {
+    return +sum * +current;
+});
 console.log(num);
-console.log(num **= 3);
-num += '';
+console.log(num = num ** 3);
+num = num.toString();
 console.log(num = num[0] + num[1]);
