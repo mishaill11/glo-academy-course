07c7da5f946209acda13ba69ad9fa7ff9fba9be9
mishaill11 <mpkhayko@mail.ru>
2019-08-26 22:20:25 +0700

    отредактировал переменные

--- lesson 3/script.js
+++ lesson 3/script.js
@@ -1,29 +1,20 @@
 'use strict';
 
-let money,
+let money= +prompt('Ваш месячный доход?'),
     income = 'Фриланс',
-    addExpenses,
-    deposit,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 500000,
-    period,
-    budgetDay,
-    budgetMonth,
-    monthCosts1,
-    monthCosts2,
-    budgetMonthCosts1,
-    budgetMonthCosts2;
+    monthCosts1 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    monthCosts2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+    budgetMonthCosts1 = +prompt('Во сколько это обойдется?'),
+    budgetMonthCosts2 = +prompt('Во сколько это обойдется?'),
+    budgetMonth  = money - ((budgetMonthCosts1) + (budgetMonthCosts2)),
+    period = Math.ceil(mission / budgetMonth),
+    budgetDay = Math.floor(budgetMonth / 30);
 
-money = prompt('Ваш месячный доход?');    
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
 addExpenses = addExpenses.split(',');
-deposit = confirm('Есть ли у вас депозит в банке?');
-monthCosts1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-budgetMonthCosts1 = prompt('Во сколько это обойдется?');
-monthCosts2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-budgetMonthCosts2 = prompt('Во сколько это обойдется?');
-budgetMonth = (Number(money) - (Number(budgetMonthCosts1) + Number(budgetMonthCosts2)));
-period = Math.ceil(mission / budgetMonth);
-budgetDay = Math.floor(budgetMonth / 30);
 
 if (budgetDay >= 800) {
     console.log('Высокий уровень дохода');
