fac0fa45b1d550f8d4928f64b9cc6fcb06ea9760
mishaill11 <mpkhayko@mail.ru>
2019-08-27 16:45:26 +0700

    усложненное задание 4 урока

--- lesson/script.js
+++ lesson/script.js
@@ -1,41 +1,18 @@
 'use strict';
 
-let money= +prompt('Ваш месячный доход?', 50000),
-    income = 'Фриланс',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    mission = 500000,
-    monthCosts1 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    budgetMonthCosts1 = +prompt('Во сколько это обойдется?'),
-    monthCosts2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    budgetMonthCosts2 = +prompt('Во сколько это обойдется?'),
-    accumulatedMonth;
+let str = '   строка с пробелами в начале и в конце возможно длиной больше 30 символов     ';
 
-function getExpensesMonth() {
-    return budgetMonthCosts1 + budgetMonthCosts2;
+function funcStr(str) {
+    if (typeof str !== typeof str.toString()) {
+        return 'Введена не строка'; 
+    } else if ((typeof str === typeof str.toString()) && (str.length >= 30)) {
+        return str.substr(0, 30).trim() + '...';
+    } else if (str.length < 30) {
+        return str.trim();
+    }
 }
 
-function getAccumulatedMonth() {
-    return money - ((budgetMonthCosts1) + (budgetMonthCosts2));
-}
-accumulatedMonth = getAccumulatedMonth();
-
-function getTargetMonth() {
-    return Math.ceil(mission / accumulatedMonth);
-}
-
-function showTypeof() {
-    return 'money: ' + typeof money + ' income: ' + typeof income +' deposit: ' + typeof deposit;
-}
-
-function getStatusIncome() {
-    return 'income length: ' + income.length;
-}
-
-console.log(getStatusIncome());
-console.log(showTypeof());
-console.log('Накопления за период: ', getTargetMonth() * accumulatedMonth);
-console.log('Период: ', getTargetMonth());
+console.log(funcStr(str));
 
 
 
