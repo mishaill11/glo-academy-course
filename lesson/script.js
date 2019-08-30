7329519421659d597851e4056bc0f38764aa16af
mishaill11 <mpkhayko@mail.ru>
2019-08-30 10:37:22 +0700

    добавил проверку на текущий день сб/вс

--- lesson/script.js
+++ lesson/script.js
@@ -3,14 +3,16 @@
 let date = new Date(),
 week = ['понедельник', 'вторник', 
 'среда', 'четверг', 'пятница', 
-'суббота', 'воскресенье']
+'суббота', 'воскресенье'];
 
 week.forEach(function(item, index) {
-    if (index === (date.getDay() - 1)){
+    if ((index === 5 || index === 6) && index === date.getDay() - 1) {
+        document.write(item.bold().italics() + '<br>');
+    }else if (index === (date.getDay() - 1)){
         document.write(item.bold() + '<br>');
     } else if (index === 5 || index === 6) {
         document.write(item.italics() + '<br>');
-    }else {
+    } else {
         document.write(item + '<br>');
     }
 });
