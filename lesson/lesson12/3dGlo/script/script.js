8301de9e22f23e6e3b3aea4d0b69714f23beb054
mishaill11 <mpkhayko@mail.ru>
2019-09-13 07:51:55 +0700

    переделан основной таймер

--- lesson/lesson12/3dGlo/script/script.js
+++ lesson/lesson12/3dGlo/script/script.js
@@ -7,13 +7,13 @@ window.addEventListener('DOMContentLoaded', () => {
             timerSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining(){
-        let dateStop = new Date('12 september 2019 11:45:00'),
+        let dateStop = new Date('13 september 2019 07:51:00'),
             dateNow = new Date().getTime(),
             timeRemaining = (dateStop - dateNow) / 1000,
             second = Math.floor(timeRemaining % 60),
             minutes = Math.floor((timeRemaining / 60) % 60),
             hours = Math.floor(timeRemaining / 60 / 60);
-            return {timeRemaining, hours, minutes, second};
+            return {timeRemaining,dateStop, hours, minutes, second};
         }
         
         function updateClock(){
@@ -22,16 +22,20 @@ window.addEventListener('DOMContentLoaded', () => {
                     timerHours.textContent = timer.hours < 10 ? '0' + timer.hours : timer.hours;
                     timerMinutes.textContent = timer.minutes < 10 ? '0'+ timer.minutes : timer.minutes;
                     timerSeconds.textContent = timer.second < 10 ? '0'+ timer.second : timer.second;
-            } if (timer.timeRemaining < 0) {
+            }
+            if (timer.timeRemaining <= 0) {
                 timerHours.nextElementSibling.style.color = 'red';
                 timerMinutes.nextElementSibling.style.color = 'red';
                 timerHours.style.color = 'red';
                 timerMinutes.style.color = 'red';
                 timerSeconds.style.color = 'red';
-                clearInterval(2);
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                clearInterval(time);
             }
         }
-        setInterval(updateClock, 1000);   
+        let time = setInterval(updateClock, 1000);   
     }
     countTimer();
 });
