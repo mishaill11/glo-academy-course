0a284b01d79c098f6c1df3054bf434a7e8316ad0
mishaill11 <mpkhayko@mail.ru>
2019-09-19 00:09:21 +0700

    таймер

--- lesson/lesson12/3dGlo/script/script.js
+++ lesson/lesson12/3dGlo/script/script.js
@@ -6,33 +6,54 @@ window.addEventListener('DOMContentLoaded', () => {
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining(){
-        let dateStop = new Date('13 september 2019 07:51:00'),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
+        function getTimeRemaining() {
+        let data = new Date().setHours(24, 0, 0, 0), 
+            dataNew = new Date().getTime();
+            
+            
+            let timeRemaining = (data - dataNew) / 1000,
             second = Math.floor(timeRemaining % 60),
             minutes = Math.floor((timeRemaining / 60) % 60),
             hours = Math.floor(timeRemaining / 60 / 60);
-            return {timeRemaining,dateStop, hours, minutes, second};
-        }
+            return {data, timeRemaining, hours, minutes, second};
+        }   
+
+        // const getTimeRemaining = () =>{
+        // let dateStop = new Date(`18 september 2019 15:12:23`),
+        //     dateNow = new Date().getTime(),
+        //     timeRemaining = (dateStop - dateNow) / 1000,
+        //     second = Math.floor(timeRemaining % 60),
+        //     minutes = Math.floor((timeRemaining / 60) % 60),
+        //     hours = Math.floor(timeRemaining / 60 / 60);
+        //     return {timeRemaining,dateStop, hours, minutes, second};
+        // }
         
-        function updateClock(){
+        function updateClock() {
             let timer = getTimeRemaining();
-            if (timer.timeRemaining > 0) {
-                    timerHours.textContent = timer.hours < 10 ? '0' + timer.hours : timer.hours;
-                    timerMinutes.textContent = timer.minutes < 10 ? '0'+ timer.minutes : timer.minutes;
-                    timerSeconds.textContent = timer.second < 10 ? '0'+ timer.second : timer.second;
-            }
+            //if (timer.timeRemaining >= 0) {
+                console.log(timer.timeRemaining);
+                (timer.hours >= 10) ? timerHours.textContent = timer.hours: timerHours.textContent = '0' + timer.hours;
+                (timer.minutes >= 10) ? timerMinutes.textContent = timer.minutes: timerMinutes.textContent = '0' + timer.minutes;
+                (timer.second >= 10) ? timerSeconds.textContent = timer.second: timerSeconds.textContent = '0' + timer.second;
+                    // timerHours.textContent = timer.hours < 10 ? '0' + timer.hours : timer.hours;
+                    // timerMinutes.textContent = timer.minutes < 10 ? '0'+ timer.minutes : timer.minutes;
+                    // timerSeconds.textContent = timer.second < 10 ? '0'+ timer.second : timer.second;
+            //}
             if (timer.timeRemaining <= 0) {
-                timerHours.nextElementSibling.style.color = 'red';
-                timerMinutes.nextElementSibling.style.color = 'red';
-                timerHours.style.color = 'red';
-                timerMinutes.style.color = 'red';
-                timerSeconds.style.color = 'red';
-                timerHours.textContent = '00';
+                
+                //timer.data.setHours('23', '59', '59');
+                console.log('timer.data: ', timer.data);
+                
+                // timerHours.nextElementSibling.style.color = 'red';
+                // timerMinutes.nextElementSibling.style.color = 'red';
+                // timerHours.style.color = 'red';
+                // timerMinutes.style.color = 'red';
+                // timerSeconds.style.color = 'red';
+                timerHours.textContent = '24';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
-                clearInterval(time);
+                
+                //clearInterval(time);
             }
         }
         let time = setInterval(updateClock, 1000);   
