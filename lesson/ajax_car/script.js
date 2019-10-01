f9b5d0dd9d0cfa8717ec90366ffa363fd49b0a35
mishaill11 <mpkhayko@mail.ru>
2019-10-01 10:07:40 +0700

    add promise

--- lesson/ajax_car/script.js
+++ lesson/ajax_car/script.js
@@ -11,10 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
             request.setRequestHeader('Content-Type', 'application/json');
             request.send();
             request.addEventListener('readystatechange', () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                        console.log(data);
-                        resolve(data);
+                if (request.readyState !== 4){
+                    return;
+                }
+                if(request.status === 200) {
+                    resolve(request);
                 } else {
                     reject();
                 }
@@ -22,7 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
            
         });
         promise
-            .then((data) => {data.cars.forEach(item => {
+            .then((request) => {
+                const data = JSON.parse(request.responseText);
+                data.cars.forEach(item => {
                 if (item.brand === select.value) {
                 const {brand, model, price} = item;
                 
