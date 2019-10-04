dd5d5cf3a5c714aaf5db94c37a6f602c049b5c17
mishaill11 <mpkhayko@mail.ru>
2019-10-04 17:34:51 +0700

    список телефонов

--- diplom_insein/src/modules/showNumber.js
+++ diplom_insein/src/modules/showNumber.js
@@ -7,10 +7,12 @@ const showNumber = () => {
 
     headerArrow.addEventListener('click', () => {
         if (bool === true){
+            headerArrow.children[0].style.transform = 'rotate(180deg)';
             phoneNumberAccord.style.top = '25px';
             phoneNumberAccord.children[0].style.opacity = 1;
             bool = false;
         } else {
+            headerArrow.children[0].style.transform = 'rotate(0deg)';
             phoneNumberAccord.style.top = '0';
             phoneNumberAccord.children[0].style.opacity = 0;
             bool = true;
