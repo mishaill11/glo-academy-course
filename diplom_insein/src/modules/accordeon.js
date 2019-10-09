76929ff503fafa89ef4dac69e5bfdd2c77e64b99
mishaill11 <mpkhayko@mail.ru>
2019-10-09 07:45:36 +0700

    аккордион

--- diplom_insein/src/modules/accordeon.js
+++ diplom_insein/src/modules/accordeon.js
@@ -3,13 +3,19 @@
 const accordeon = () => {
     const accordion = document.querySelector('.accordion'),
     titleBlock = document.querySelectorAll('.title_block');
-
+    let bool = true;
     accordion.addEventListener('click', event => {
         let target = event.target;
         if (target.closest('.title_block')) {
             titleBlock.forEach(elem => {
                 if (target === elem) {
-                    elem.classList.add('msg-active');
+                    if (bool === true){
+                        elem.classList.remove('msg-active');
+                        bool = false;
+                    } else if (bool === false) {
+                        elem.classList.add('msg-active');
+                        bool = true;
+                    }
                 } else {
                     elem.classList.remove('msg-active'); 
                 }
